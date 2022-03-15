6fd1d9556a4d484bba1de394388eeaefc2f0044c
Piotr162991 <100698056+Piotr162991@users.noreply.github.com>
2022-03-15 14:32:21 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.lang.Math;
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("podaj stopnie celsjiusz");
+    int cel = 1;
+     for (int i = 1; i <= 30; i++)
+       {
+         far= cel*9/5+23;
+         cel+10;
+         System.out.println(far);
+       }
+       
+  }
+}
